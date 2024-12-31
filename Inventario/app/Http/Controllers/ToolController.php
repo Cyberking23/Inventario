01932d959ec33d103783bb57301ba792352c18af
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-12-31 12:04:56 -0600

    Inicio de Inventario
    Inicio de Inventario

---
+++ Inventario/app/Http/Controllers/ToolController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tool;
+use Illuminate\Http\Request;
+
+class ToolController extends Controller
+{
+    public function show (){
+        
+    }
+
+    public function store (){
+        
+    }
+
+    public function update (){
+        
+    }
+
+    public function destroy (){
+        
+    }
+}
