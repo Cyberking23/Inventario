b5350b72a48979be653091b8047b3d040eca2ac3
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2025-01-02 18:21:19 -0600

    Rondeado
    Rondeado

--- Inventario/resources/views/Login.blade.php
+++ Inventario/resources/views/Login.blade.php
@@ -10,7 +10,7 @@
 <body>
     <div class=" h-screen bg-blue-500 flex items-center justify-center">
         <div class="flex">
-            <div class=" bg-white w-[500px]  p-10 ">
+            <div class=" bg-white w-[500px] p-10 rounded-s-xl ">
                 <div class="flex items-center justify-center   ">
                     <div>
                         <img src="{{ asset('images/logo.jpg') }}" class="w-10" alt="">
@@ -51,8 +51,8 @@
                     <h1>Dont have account <span class="text-red-500">Sign Up</span></h1>   
                 </div>
             </div>
-            <div>
-                <img src="{{ asset('images/Trabajo.jpg') }}" class="h-full w-[600px]" alt="">
+            <div >
+                <img src="{{ asset('images/Trabajo.jpg') }}" class="h-full w-[600px] rounded-r-lg" alt="">
             </div>
         </div>
     </div>
