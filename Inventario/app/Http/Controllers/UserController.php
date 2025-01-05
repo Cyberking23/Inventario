64994655b2d0b7909e0c3bfa011bd7768dd17929
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2025-01-05 12:47:09 -0600

    Modifciacion de proyecto
    Modifciacion de proyecto

--- Inventario/app/Http/Controllers/UserController.php
+++ Inventario/app/Http/Controllers/UserController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Middleware\CheckEmailExists;
 
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        // Aplica el middleware 'check.email.exists' solo en las rutas que manejan la creación de usuarios
+        $this->middleware('check.email.exists')->only(['store']);
+    }
 
     public function index()
     {
@@ -19,12 +26,16 @@ class UserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email|max:255',
-            'password' => 'required|string|min:8|confirmed',
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:8',
         ]);
-    
-         $user = User::create($request->all());
-    
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
         return response()->json($user, 201); // 201 indica que se creó un recurso
     }
 }
---
+++ Inventario/app/Http/Middleware/CheckEmailExists.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckEmailExists
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Verifica si ya existe un usuario con el correo proporcionado
+        $emailExists = User::where('email', $request->email)->exists();
+
+        if ($emailExists) {
+            // Si el correo ya existe, devuelve una respuesta de error
+            return response()->json(['error' => 'El correo electrónico ya está registrado.'], 400);
+        }
+
+        // Si no existe, continúa con la solicitud
+        return $next($request);
+    }
+    
+}
