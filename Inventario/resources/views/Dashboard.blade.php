9e45e31ccac141ff8942bfc40167dafa9cf8ceb9
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2025-01-12 09:45:11 -0600

    Modificacion pequeña
    Modificacion pequeña

--- Inventario/resources/views/Dashboard.blade.php
+++ Inventario/resources/views/Dashboard.blade.php
@@ -56,33 +56,7 @@
 
             <!-- Tabla de órdenes -->
             <div class="bg-white rounded-lg shadow">
-                <table class="min-w-full divide-y divide-gray-200">
-                    <thead>
-                        <tr class="bg-gray-50">
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                N°
-                            </th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Nombre Producto
-                            </th>
-                    
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Cantidad
-                            </th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Estado
-                            </th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Ubicacion
-                            </th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Acciones
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                      
-                    </tbody>
+               
 
                 </table>
             </div>
